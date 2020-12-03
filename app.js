d2ce7650d75bf1c9eb39bee043d1737afe8df218
sahilrastogi25 <sahil.rstg2598@gmail.com>
2020-12-03 11:37:17 +0530

    Added gitignore,procfile and updated ports

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
